81e55954c7b444a39b1134c461cad79cd9a06203
Alex Padilla <alex.padilla@epn.edu.ec>
2023-08-22 16:24:40 -0500

    feat(reservas): implementacion del submodulo reservas, con datos de prueba

--- src/Hospedaje/Reservas/Gestion_Reservas.java
+++ src/Hospedaje/Reservas/Gestion_Reservas.java
@@ -1,4 +1,29 @@
 package Hospedaje.Reservas;
 
-public class GestionReservas {
+import Hospedaje.Habitaciones.Gestion_Habitaciones;
+import Hospedaje.Habitaciones.Habitacion;
+import Reservas.Reserva;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Gestion_Reservas {
+    private List<ReservaHospedaje> reservas;
+
+    public Gestion_Reservas() {
+        this.reservas = new ArrayList<>();
+        Date fechaCreacion = new Date();
+        Date fechaInicio = new Date();
+        Date fechaFin = new Date();
+        Habitacion habitacion1 = Gestion_Habitaciones.getHabitaciones().get(1);
+        Habitacion habitacion2 = Gestion_Habitaciones.getHabitaciones().get(2);
+        Habitacion[] habitaciones = {habitacion1, habitacion2};
+
+        this.reservas.add(new ReservaHospedaje("1" , "1", 2, habitaciones, fechaCreacion, fechaInicio, fechaFin));
+    }
+
+    public List<ReservaHospedaje> getReservas() {
+        return reservas;
+    }
 }
--- src/Hospedaje/Reservas/ReservaHospedaje.java
+++ src/Hospedaje/Reservas/ReservaHospedaje.java
@@ -1,4 +1,57 @@
 package Hospedaje.Reservas;
 
-public class ReservaHospedaje {
+import Hospedaje.Habitaciones.Habitacion;
+import Reservas.Reserva;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ReservaHospedaje extends Reserva {
+    private int numeroPersonas;
+    private Date fechaCreacion;
+    private Date fechaCancelacionPago;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private Habitacion[] habitaciones;
+    ReservaHospedaje(String userId, String reservaId, int numeroPersonas, Habitacion[] habitaciones, Date fechaCreacion, Date fechaInicio, Date fechaFin) {
+        super(userId, reservaId);
+        this.numeroPersonas = numeroPersonas;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.habitaciones = habitaciones;
+    }
+
+    @Override
+    public void cancelarReserva() {
+
+    }
+
+    @Override
+    public void modificarReserva() {
+
+    }
+
+    public String getInfo() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        StringBuilder habitacionesInfo = new StringBuilder();
+        habitacionesInfo.append("\nHabitaciones: ");
+        for (Habitacion habitacion : habitaciones) {
+            habitacionesInfo.append(habitacion.getInfo());
+        }
+
+        return "ReservaHospedaje:" +
+                "\n\tnumeroPersonas=" + numeroPersonas +
+                "\n\tfechaCreacion=" + dateFormat.format(fechaCreacion) +
+                //"\nfechaCancelacionPago=" + dateFormat.format(fechaCancelacionPago) +
+                "\n\tfechaInicio=" + dateFormat.format(fechaInicio) +
+                "\n\tfechaFin=" + dateFormat.format(fechaFin) +
+                "\n\tHabitaciones:" + habitacionesInfo.toString() +
+                "\n";
+    }
+    public boolean confirmarReserva() {
+        // pagar
+        return true;
+    }
 }
