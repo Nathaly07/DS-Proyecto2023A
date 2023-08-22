d8ffa7acfa347fae6989c80486a23b9bd15c6756
Chris <chris.athan12@gmail.com>
2023-08-22 18:37:24 -0500

    metodo cancelar

--- src/Tours/Gestion_Reserva.java
+++ src/Tours/Gestion_Reserva.java
@@ -1,15 +1,28 @@
 package Tours;
+import Reservas.Reserva;
 import Reservas.ReservaTour;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Gestion_Reserva {
 
-    private ReservaTour[] reservas;
+    private List<ReservaTour> reservas;
 
     public Gestion_Reserva() {
-
+        this.reservas = new ArrayList<>();
     }
-    public void cancelar(){
-
+    public void cancelar(String id){
+        ReservaTour reservaACancelar = null;
+        for (ReservaTour reserva : this.reservas) {
+            if (reserva.getReservaID().equalsIgnoreCase(id)) {
+                reservaACancelar = reserva;
+                break;
+            }
+        }
+        if (reservaACancelar != null) {
+            this.reservas.remove(reservaACancelar);
+        }
     }
     public void actualizar(){
 
