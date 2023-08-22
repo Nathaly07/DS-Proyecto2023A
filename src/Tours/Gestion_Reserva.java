3cf999d05f24093a0d02e5aeba32dfa9a54ec0bc
Chris <chris.athan12@gmail.com>
2023-08-22 16:42:34 -0500

    Corrección de ubicacion de clase "Gestion_Reserva"

---
+++ src/Tours/Gestion_Reserva.java
@@ -0,0 +1,22 @@
+package Tours;
+
+public class Gestion_Reserva {
+
+    //private Reserva[] reservas;
+
+    public Gestion_Reserva() {
+
+    }
+    public void cancelar(){
+
+    }
+    public void actualizar(){
+
+    }
+//  public void totalPersonasPorTour(tourID){
+//
+//  }
+    public void agregarReserva(){
+
+    }
+}
