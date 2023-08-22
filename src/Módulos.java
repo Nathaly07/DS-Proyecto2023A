41641d5f04e22a911335fd6ea505a2160f9cc1e5
Sebastián <alejandro.jimenez@epn.edu.ec>
2023-08-21 21:11:50 -0500

    feat: agregado del módulo seguros al repositorio

--- src/Módulos.java
+++ src/Módulos.java
@@ -1,18 +1,29 @@
+import Seguros.InterfazSeguros;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class Módulos extends JFrame {
     private JPanel pnlMódulos;
-    private JButton btnMódulo1;
-    private JButton btnMódulo2;
-    private JButton btnMódulo3;
-    private JButton btnMódulon;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton btnSeguros;
     private JButton btnExit;
+    private JPanel pnlContenido;
+
 
     public Módulos() {
         btnExit.addActionListener(e -> {
             Login login = new Login();
             login.crearFrame();
             dispose();
+        });
+        btnSeguros.addActionListener(e -> {
+            InterfazSeguros interfazSeguros = new InterfazSeguros();
+            setPanel(interfazSeguros.pnlOpcionesSeguro);
+            crearFrame();
         });
     }
 
@@ -23,5 +34,9 @@ public class Módulos extends JFrame {
         setLocationRelativeTo(null);
         add(pnlMódulos);
         setVisible(true);
+    }
+
+    public void setPanel(JPanel pnlMódulos) {
+        pnlContenido.add(pnlMódulos);
     }
 }
---
+++ src/Seguros/Seguro.java
@@ -0,0 +1,74 @@
+package Seguros;
+
+import java.util.Date;
+
+public abstract class Seguro {
+    private int ID_Seguro;
+    private String propietario;
+    private String[] condiciones;
+    private String[] beneficiarios;
+    private Date fechaDeInicio;
+    private Date fechaDeVencimiento;
+
+    private float primaSinRecargo;
+
+
+    public Seguro(int ID_Seguro, String propietario, String[] condiciones, String[] beneficiarios, Date fechaDeInicio, Date fechaDeVencimiento, float primaSinRecargo) {
+        this.ID_Seguro = ID_Seguro;
+        this.propietario = propietario;
+        this.condiciones = condiciones;
+        this.beneficiarios = beneficiarios;
+        this.fechaDeInicio = fechaDeInicio;
+        this.fechaDeVencimiento = fechaDeVencimiento;
+        this.primaSinRecargo = primaSinRecargo;
+    }
+
+    public boolean renovar(Date fecha) {
+        if (fecha.after(fechaDeVencimiento)) {
+            this.fechaDeVencimiento = fecha;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void cancelar(int ID_Seguro) {
+        this.ID_Seguro = 0;
+    }
+
+    abstract float indemnizar(float valorGastado, String motivo);
+
+    abstract float calcularPrimaTotal();
+
+    public int getID_Seguro() {
+        return ID_Seguro;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public String[] getCondiciones() {
+        return condiciones;
+    }
+
+    public String[] getBeneficiarios() {
+        return beneficiarios;
+    }
+
+    public Date getFechaDeInicio() {
+        return fechaDeInicio;
+    }
+
+    public Date getFechaDeVencimiento() {
+        return fechaDeVencimiento;
+    }
+
+    public float getPrimaSinRecargo() {
+        return primaSinRecargo;
+    }
+
+    public void setFechaDeVencimiento(Date fechaDeVencimiento) {
+        this.fechaDeVencimiento = fechaDeVencimiento;
+    }
+}
