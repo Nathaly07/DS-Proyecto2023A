6e048526f13e79b50f4a8d40da921a46205fc40e
Alex Padilla <alex.padilla@epn.edu.ec>
2023-08-23 01:21:21 -0500

    feat(hospedaje): integrar modulo hospedaje con los submodulos

--- src/Hospedaje/ModuloHospedaje.java
+++ src/Hospedaje/ModuloHospedaje.java
@@ -1,2 +1,39 @@
-package Hospedaje;public class ModuloHospedaje {
+package Hospedaje;
+
+import Hospedaje.Habitaciones.Gestion_Habitaciones;
+import Hospedaje.Habitaciones.Habitacion;
+import Hospedaje.Reservas.Gestion_Reservas;
+import Hospedaje.Reservas.ReservaHospedaje;
+
+import java.util.Date;
+import java.util.List;
+
+public class ModuloHospedaje {
+    private Gestion_Habitaciones gestion_habitaciones;
+    private Gestion_Reservas gestion_reservas;
+
+    public ModuloHospedaje() {
+        this.gestion_habitaciones = new Gestion_Habitaciones();
+        this.gestion_reservas = new Gestion_Reservas(gestion_habitaciones);
+    }
+
+    public boolean crearReserva(ReservaHospedaje reserva) {
+        return this.gestion_reservas.crearReserva(reserva);
+    }
+
+    public boolean actualizarReserva(String reservaId, ReservaHospedaje nuevaReserva) {
+        return this.gestion_reservas.actualizarReserva(reservaId, nuevaReserva);
+    }
+
+    public boolean cancelarReserva(String reservaId) {
+        return this.gestion_reservas.cancelarReserva(reservaId);
+    }
+
+    public boolean confirmarReserva(String reservaID, String metodoPago) {
+        return this.gestion_reservas.confirmarReserva(reservaID, metodoPago);
+    }
+
+    public List<Habitacion> buscarHabitacionesDisponibles(Date reservarDesde, Date reservarHasta) {
+        return this.gestion_reservas.getHabitacionesDisponibles(reservarDesde, reservarHasta);
+    }
 }
--- src/Hospedaje/Pagos/PagoHospedaje.java
+++ src/Hospedaje/Pagos/PagoHospedaje.java
@@ -1,9 +1,26 @@
 package Hospedaje.Pagos;
 
+import Hospedaje.Reservas.ReservaHospedaje;
 import Pagos.Pago;
 
+import javax.swing.*;
+
 public class PagoHospedaje extends Pago {
-    public PagoHospedaje(double montoAPagar, String modoPago) {
-        super(montoAPagar, modoPago);
+    private ReservaHospedaje reservaHospedaje;
+
+    public PagoHospedaje(ReservaHospedaje reservaHospedaje, String metodoPago) {
+        super(calcularMontoTotal(reservaHospedaje), metodoPago);
+        this.reservaHospedaje = reservaHospedaje;
+    }
+
+    private static double calcularMontoTotal(ReservaHospedaje reserva) {
+        long dias = (reserva.getFechaFin().getTime() - reserva.getFechaInicio().getTime()) / (24 * 60 * 60 * 1000);
+        double montoTotal = dias * reserva.getHabitaciones().length * reserva.getHabitaciones()[0].getPrecioPorNoche();
+        return montoTotal;
+    }
+
+    @Override
+    public void pagar() {
     }
 }
+
