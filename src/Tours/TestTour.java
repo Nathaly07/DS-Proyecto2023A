6fed2065930f8eaafbb22ad4b2df37b9076add48
dacol <da.colcha@hotmail.com>
2023-08-23 00:55:02 -0500

    int -> void

--- src/Tours/TestTour.java
+++ src/Tours/TestTour.java
@@ -5,12 +5,36 @@ public class TestTour {
     public TestTour() {
     }
 
-    public int initTour() {
+    public void initTour() {
         int op = Integer.parseInt(
             JOptionPane.showInputDialog(null, 
             "1. Buscar Tours \n2. Reservar Tour\n3. Modificar Tour\n4. Eliminar Tour\n5. Confirmar Tour\n6. Salir\n ", 
             JOptionPane.QUESTION_MESSAGE));
 
-            return op;
+        
+        switch (op) {         
+            case 1:
+                JOptionPane.showMessageDialog(null, "Buscar Tours");
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "Reservar Tour");
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null, "Modificar Tour");
+                break;
+            case 4:
+                JOptionPane.showMessageDialog(null, "Eliminar Tour");
+                break;
+            case 5:
+                JOptionPane.showMessageDialog(null, "Confirmar Tour");
+                break;
+            case 6:
+                JOptionPane.showMessageDialog(null, "Salir");
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opción no válida");
+                break;
+        }
+
     }
 }
