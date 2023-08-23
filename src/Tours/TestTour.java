6fa01414a12efac35ef2111629cd1bcbe319348f
dacol <da.colcha@hotmail.com>
2023-08-23 00:48:06 -0500

    Feature

--- src/Tours/TestTour.java
+++ src/Tours/TestTour.java
@@ -5,12 +5,8 @@ public class TestTour {
         //Probando
         
         int op = Integer.parseInt(
-                JOptionPane.showInputDialog("1. Buscar Tours \n
-                                            2. Reservar Tour\n
-                                            3. Modificar Tour\n
-                                            4. Eliminar Tour\n
-                                            5. Confirmar Tour\n
-                                            6. Salir\n", JOptionPane.QUESTION_MESSAGE));
-        );
+            JOptionPane.showInputDialog(null, 
+            "1. Buscar Tours \n2. Reservar Tour\n3. Modificar Tour\n4. Eliminar Tour\n5. Confirmar Tour\n6. Salir\n ", 
+            JOptionPane.QUESTION_MESSAGE));
     }
 }
