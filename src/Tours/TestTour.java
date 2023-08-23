8eb4d383f2ff2f6593208736570d79448fe81e25
dacol <da.colcha@hotmail.com>
2023-08-23 00:43:30 -0500

    Creacion Menu

---
+++ src/Tours/TestTour.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class TestTour {
+    public static void main(String[] args) {
+        //Probando
+        
+        int op = Integer.parseInt(
+                JOptionPane.showInputDialog("1. Buscar Tours \n
+                                            2. Reservar Tour\n
+                                            3. Modificar Tour\n
+                                            4. Eliminar Tour\n
+                                            5. Confirmar Tour\n
+                                            6. Salir\n", JOptionPane.QUESTION_MESSAGE));
+        );
+    }
+}
