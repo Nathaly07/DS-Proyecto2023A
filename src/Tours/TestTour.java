d8614e976d2e7ab30c69868e02d898ec59c3e244
dacol <da.colcha@hotmail.com>
2023-08-23 00:51:10 -0500

    Feature menu

--- src/Tours/TestTour.java
+++ src/Tours/TestTour.java
@@ -1,12 +1,16 @@
 import javax.swing.JOptionPane;
 
 public class TestTour {
-    public static void main(String[] args) {
-        //Probando
-        
+
+    public TestTour() {
+    }
+
+    public int initTour() {
         int op = Integer.parseInt(
             JOptionPane.showInputDialog(null, 
             "1. Buscar Tours \n2. Reservar Tour\n3. Modificar Tour\n4. Eliminar Tour\n5. Confirmar Tour\n6. Salir\n ", 
             JOptionPane.QUESTION_MESSAGE));
+
+            return op;
     }
 }
