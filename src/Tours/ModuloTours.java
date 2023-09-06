167e72b82e1e1b0b9bfee2ec6cc6e2e659da3c82
Martín Mendieta <martin.mendieta@epn.edu.ec>
2023-09-06 02:00:05 -0500

    Agregar funcionalidad a Crear y Modificar reserva

--- src/Tours/ModuloTours.java
+++ src/Tours/ModuloTours.java
@@ -57,9 +57,15 @@ public class ModuloTours extends JFrame{
     private JPanel pnlTourList;
     private JPanel pnlToursDisponiblesModificar;
     private JPanel pnlListaToursReservaModificar;
+    private JButton btnAgregarTourModificar;
+    private JButton btnEliminarTourModificar;
+    private JList listToursReservaModificar;
+    private JButton btnModificarReserva;
+    private JSpinner jspNumPersonasModificar;
     private Tour tour; //para prueba
     private ReservaTour reservaTour;
     private ReservaTour reservaTourConfirmar = null;
+    private ReservaTour reservaTourModificar = null;
     private String metodoPagoConfirmar = "";
 
     private ArrayList<Tour> listatemp = new ArrayList<>();
@@ -70,64 +76,6 @@ public class ModuloTours extends JFrame{
         super (head);
         this.usuarioVerificado = usuarioVerificado;
 
-//        panelTours.setVisible(false);
-//        panelReserva.setVisible(false);
-//        mostrarToursDisponiblesButton.addActionListener(e -> {
-//            panelTours.setVisible(true);
-//            this.buscarToursDisponibles();
-//        });
-//        verReservaButton.addActionListener(e -> {
-//            panelReserva.setVisible(true);
-//            this.verReserva();
-//        });
-//
-//
-//        confirmarReservaButton.addActionListener(new ActionListener() {
-//
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                JOptionPane.showMessageDialog(null, "Confirmar Reserva");
-//                String idReserva = JOptionPane.showInputDialog("Ingrese el id de la reserva");
-//                String metodoPago = JOptionPane.showInputDialog("Seleccione el metodo de pago:\n-Paypal\n-Transferncia\n-Tarjeta de credito");
-//                ReservaTour reserva = gestionReserva.getReserva(idReserva);
-//
-//                SimpleDateFormat format = new SimpleDateFormat("dd/M/yy");
-//                LocalDate fechaActual = LocalDate.now();
-//                if (reserva == null){
-//                    JOptionPane.showMessageDialog(null, "Reserva no encontrada");
-//                } else {
-//                    reserva.setFechaConfirmacion(fechaActual.toString());
-//
-//                    PagoReserva pagoTour = new PagoReserva(reserva);
-//                    double valorTotal = pagoTour.calcularPrecioFinal();
-//                    Pago pago = new Pago(valorTotal, metodoPago);
-//                    pago.pagar();
-//                }
-//            }
-//        });
-//
-//        cancelarReservaButton.addActionListener(new ActionListener() {
-//
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                JOptionPane.showMessageDialog(null, "Cancelar Reserva");
-//                String idReserva = JOptionPane.showInputDialog("Ingrese el id de la reserva");
-//                ReservaTour reserva = gestionReserva.getReserva(idReserva);
-//
-//                if (reserva == null){
-//                    JOptionPane.showMessageDialog(null, "Reserva no encontrada");
-//                } else {
-//                    PagoReserva pagoTour = new PagoReserva(reserva);
-//                    double valorDevolucion = pagoTour.calcularDevolucion();
-//                    gestionReserva.cancelar(idReserva);
-//                    JOptionPane.showMessageDialog(null,
-//                            "La reserva se ha eliminado correctamente. /n" +
-//                                    "El valor a devolver es: " + valorDevolucion,
-//                            "Reserva Tour",
-//                            JOptionPane.WARNING_MESSAGE);
-//                }
-//            }
-//        });
         //Listener para mostrar los tours
         pnlCrearReserva.addComponentListener(new ComponentAdapter() {
             @Override
@@ -147,7 +95,7 @@ public class ModuloTours extends JFrame{
                 } catch (ParseException ex) {
                     throw new RuntimeException(ex);
                 }
-                mostrarReserva(list1);
+                mostrarReserva(list2);
             }
         });
 
@@ -155,7 +103,7 @@ public class ModuloTours extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 eliminarTour();
-                mostrarReserva(list1);
+                mostrarReserva(list2);
             }
         });
 
@@ -216,6 +164,50 @@ public class ModuloTours extends JFrame{
                 metodoPagoConfirmar = efectivoRadioButton.getText();
             }
         });
+        comboBox3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String opcion = (String)comboBox3.getSelectedItem();
+                int numReserva = Integer.parseInt(opcion.split("-")[0]);
+                reservaTourModificar = gestionReserva.buscarReserva(numReserva);
+                DefaultListModel toursModel = new DefaultListModel<>();
+                ArrayList<Tour> toursList = (ArrayList<Tour>) gestionTour.getToursDisponibles();
+
+                for(Tour tour: toursList) {
+                    toursModel.addElement(tour.informacionRelevante());
+                }
+
+                listToursModificar.setModel(toursModel);
+
+            }
+        });
+        btnAgregarTourModificar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String tours = listToursModificar.getSelectedValue().toString();
+                String[] nombreTour = tours.split(",");
+                String Ntour = nombreTour[0];
+                listatemp.add(gestionTour.buscarTour(Ntour));
+                mostrarReserva(listToursReservaModificar);
+            }
+        });
+        btnEliminarTourModificar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String tours = listToursReservaModificar.getSelectedValue().toString();
+                String[] nombreTour = tours.split(",");
+                String Ntour = nombreTour[0];
+                listatemp.add(gestionTour.buscarTour(Ntour));
+                mostrarReserva(listToursReservaModificar);
+            }
+        });
+        btnModificarReserva.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                reservaTourModificar.setToursAgregados(listatemp);
+                reservaTourModificar.setNumeroPersonas((Integer)jspNumPersonasModificar.getValue());
+            }
+        });
     }
 
 
@@ -254,7 +246,7 @@ public class ModuloTours extends JFrame{
 
     public void mostrarReserva(JList list){
         DefaultListModel<String> model = new DefaultListModel<>();
-        ArrayList<Tour> tours = this.reservaTour.getToursAgregados();
+        ArrayList<Tour> tours = this.listatemp;
 
         for(Tour i: tours){
             model.addElement(i.informacionRelevante());
@@ -278,118 +270,7 @@ public class ModuloTours extends JFrame{
     }
 
     public void crearReserva(){
-        reservaTour = new ReservaTour(this.usuarioVerificado.getNombre(), this.usuarioVerificado.getApellido(), (int)this.spinner1.getValue() ,this.gestionTour, this.pagoReserva);
+        reservaTour = new ReservaTour(this.usuarioVerificado.getNombre(), this.usuarioVerificado.getApellido(), (Integer) this.spinner1.getValue() ,this.gestionTour, this.pagoReserva);
     }
-
-   /* public void buscarToursDisponibles(){
-        int yPos = 10;
-
-        Tour tour1 = new Tour("01", "Tour 1", 100.2, "Guia", "1 mes", 100, gestionReserva);
-        Tour tour2 = new Tour("02", "Tour 2", 100.2, "Guia", "1 mes", 100, gestionReserva);
-        this.gestionTour.nuevoTour(tour1);
-        this.gestionTour.nuevoTour(tour2);
-        this.tours = this.gestionTour.getToursDisponibles();
-        for(Tour aux: this.tours){
-
-
-            JLabel nombreLabel = new JLabel("Nombre: ");
-            JTextField nombreTF = new JTextField(aux.getNombre());
-            JLabel precioLabel = new JLabel("Precio: ");
-            JTextField precioTF = new JTextField(String.valueOf(aux.getPrecio()));
-            JLabel infoGuiaLabel = new JLabel("Guia: ");
-            JTextField infoGuiaTF = new JTextField(aux.getInfo_guia());
-            JLabel duracionLabel = new JLabel("Duracion: ");
-            JTextField duracionTF = new JTextField(aux.getDuracion());
-            JLabel limiteUsuariosLabel = new JLabel("Limite usuarios: ");
-            JTextField limiteUsuariosTF = new JTextField(aux.getLimiteUsuarios());
-
-            JButton botonReservar = new JButton("Reservar");
-
-            botonReservar.addActionListener(e -> {
-                ArrayList<Tour> toursAgregados = null;
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                LocalDate fechaActual = LocalDate.now();
-                this.reservaTour = new ReservaTour("01","01", gestionTour, fechaActual.toString(), 100, true, toursAgregados);
-                reservaTour.agregarTour(aux);
-                gestionReserva.agregarReserva(reservaTour);
-            });
-
-            nombreLabel.setBounds(10, yPos, 100, 25);
-            nombreTF.setBounds(110, yPos, 150, 25);
-            precioLabel.setBounds(10, yPos + 30, 100, 25);
-            precioTF.setBounds(110, yPos + 30, 60, 25);
-            infoGuiaLabel.setBounds(10, yPos + 60, 100, 25);
-            infoGuiaTF.setBounds(110, yPos + 60, 150, 25);
-            duracionLabel.setBounds(10, yPos + 90, 100, 25);
-            duracionTF.setBounds(110, yPos + 90, 150, 25);
-            limiteUsuariosLabel.setBounds(10, yPos + 120, 100, 25);
-            limiteUsuariosTF.setBounds(110, yPos + 120, 60, 25);
-            botonReservar.setBounds(10, yPos + 150, 100, 25);
-
-            panelTours.add(nombreLabel);
-            panelTours.add(nombreTF);
-            panelTours.add(precioLabel);
-            panelTours.add(precioTF);
-            panelTours.add(infoGuiaLabel);
-            panelTours.add(infoGuiaTF);
-            panelTours.add(duracionLabel);
-            panelTours.add(duracionTF);
-            panelTours.add(limiteUsuariosLabel);
-            panelTours.add(limiteUsuariosTF);
-            panelTours.add(botonReservar);
-
-            yPos += 140;
-        }
-
-
-    }
-    public void verReserva(){
-        this.tours = this.reservaTour.getToursAgregados();
-        panelReserva.setVisible(true);
-        int yPos = 10;
-        for(Tour aux: this.reservaTour.getToursAgregados()){
-            JLabel nombreLabel = new JLabel("Nombre: ");
-            JTextField nombreTF = new JTextField(aux.getNombre());
-            JLabel precioLabel = new JLabel("Precio: ");
-            JTextField precioTF = new JTextField(String.valueOf(aux.getPrecio()));
-            JLabel infoGuiaLabel = new JLabel("Guia: ");
-            JTextField infoGuiaTF = new JTextField(aux.getInfo_guia());
-            JLabel duracionLabel = new JLabel("Duracion: ");
-            JTextField duracionTF = new JTextField(aux.getDuracion());
-            JLabel limiteUsuariosLabel = new JLabel("Limite usuarios: ");
-            JTextField limiteUsuariosTF = new JTextField(aux.getLimiteUsuarios());
-
-
-            nombreLabel.setBounds(10, yPos, 100, 25);
-            nombreTF.setBounds(110, yPos, 150, 25);
-            precioLabel.setBounds(10, yPos + 30, 100, 25);
-            precioTF.setBounds(110, yPos + 30, 60, 25);
-            infoGuiaLabel.setBounds(10, yPos + 60, 100, 25);
-            infoGuiaTF.setBounds(110, yPos + 60, 150, 25);
-            duracionLabel.setBounds(10, yPos + 90, 100, 25);
-            duracionTF.setBounds(110, yPos + 90, 150, 25);
-            limiteUsuariosLabel.setBounds(10, yPos + 120, 100, 25);
-            limiteUsuariosTF.setBounds(110, yPos + 120, 60, 25);
-
-            panelReserva.add(nombreLabel);
-            panelReserva.add(nombreTF);
-            panelReserva.add(precioLabel);
-            panelReserva.add(precioTF);
-            panelReserva.add(infoGuiaLabel);
-            panelReserva.add(infoGuiaTF);
-            panelReserva.add(duracionLabel);
-            panelReserva.add(duracionTF);
-            panelReserva.add(limiteUsuariosLabel);
-            panelReserva.add(limiteUsuariosTF);
-
-
-            yPos += 140;
-        }
-        JButton botonAceptar = new JButton("Aceptar");
-        botonAceptar.addActionListener(e -> {
-            JOptionPane.showMessageDialog(null,"Aceptado Existosamente");
-        });
-        panelReserva.add(botonAceptar);
-    }*/
 
 }
